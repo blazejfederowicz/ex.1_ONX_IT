cffc34802ce15c3b6cc79b77cda0bb1e8da5b946
Błażej <wargun364@gmail.com>
2025-01-31 18:35:16 +0100

    routes

---
+++ Frontend/src/router.js
@@ -0,0 +1,22 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import DefaultComponent from "./components/DefaultLayout.vue";
+import HomePage from "./pages/HomePage.vue";
+import UrlsPage from "./pages/UrlsPage.vue";
+
+const routes = {
+    path:'/',
+    component: DefaultComponent,
+    children:[
+        {path:'/', name:'Home', component: HomePage },
+        {path:'/shorturl', name: 'Url', component: UrlsPage}
+    ]
+}
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+
+
+export default router;
