23782b48a44b4ef69354102c7d1fec53680e8b7a
Błażej <wargun364@gmail.com>
2025-01-31 21:04:12 +0100

    css tailwind

--- Frontend/src/components/DefaultLayout.vue
+++ Frontend/src/components/DefaultLayout.vue
@@ -1,9 +1,50 @@
 <template>
-    <slot><RouterView/></slot>
-</template>
-<script setup>
-    import { RouterView } from 'vue-router';
-</script>
-<style scoped>
-    
-</style>
+    <div class="min-h-full">
+      <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div class="flex h-16 items-center justify-between">
+            <div class="flex items-center">
+              <div class="shrink-0">
+                <img class="size-8" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+              </div>
+              <div class="hidden md:block">
+                <div class="ml-10 flex items-baseline space-x-4">
+                  <RouterLink v-for="item in navigation" :key="item.name" :to="item.to" :class="[$route.name === item.to.name ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']" :aria-current="$route.name === item.to.name  ? 'page' : undefined">{{ item.name }}</RouterLink>
+                </div>
+              </div>
+            </div>
+            <div class="-mr-2 flex md:hidden">
+              <!-- Mobile menu button -->
+              <DisclosureButton class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
+                <span class="absolute -inset-0.5" />
+                <span class="sr-only">Open main menu</span>
+                <Bars3Icon v-if="!open" class="block size-6" aria-hidden="true" />
+                <XMarkIcon v-else class="block size-6" aria-hidden="true" />
+              </DisclosureButton>
+            </div>
+          </div>
+        </div>
+        <DisclosurePanel class="md:hidden">
+          <div class="space-y-1 px-2 pt-4 pb-3 sm:px-3 border-t border-gray-700">
+            <RouterLink v-for="item in navigation" :key="item.name" :to="item.to" :class="[$route.name === item.to.name ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="$route.name === item.to.name ? 'page' : undefined">{{ item.name }}</RouterLink>
+          </div>
+        </DisclosurePanel>
+      </Disclosure>
+      <main class="">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <RouterView/>
+        </div>
+      </main>
+    </div>
+  </template>
+  
+  <script setup>
+    import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
+    import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+    import { RouterLink, RouterView } from 'vue-router';
+  
+    const navigation = [
+        { name: 'Home', to: {name: 'Home'} },
+        { name: "Your URL", to:{name:'Url'} },
+    ]
+  </script>
--- Frontend/src/pages/HomePage.vue
+++ Frontend/src/pages/HomePage.vue
@@ -1,11 +1,31 @@
 <template>
-    <DefaultLayout>
-        home
-    </DefaultLayout>
-</template>
+    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm ">
+        <form class="space-y-6">
+          <div>
+            <label for="url" class="block text-sm/6 font-medium text-gray-900">Enter URL</label>
+            <div class="mt-2">
+              <input  name="url" id="url" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            </div>
+          </div>
+          <div>
+            <p class="block text-sm/6 font-medium text-gray-900">Short URL</p>
+            <div class="mt-2">
+              <div  class="block w-full rounded-md bg-gray-200 px-3 py-1.5  sm:text-sm/6" >
+                <a href="#" class=" underline underline-offset-2 text-blue-500">fdfd</a>
+              </div>
+            </div>
+          </div>
+          <div>
+            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Convert</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </template>
+  
 <script setup>
 
-import DefaultLayout from '../components/DefaultLayout.vue';
 
 </script>
 <style scoped>
