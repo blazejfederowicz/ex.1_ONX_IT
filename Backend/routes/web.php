da9c78d01c4147d1b2524e93702beb25f5865d77
Błażej <wargun364@gmail.com>
2025-02-01 02:39:26 +0100

    redirecting short links

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -1,9 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Url;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-require __DIR__.'/auth.php';
+Route::get('/{shortened_url}', function ($shortened_url) {
+    $url = Url::where('shortener_url', 'like', '%/' . $shortened_url)->first();
+
+    if ($url) {
+        return redirect()->to($url->original_url);
+    }
+
+    abort(404, 'URL not found');
+});
+
+require __DIR__ . '/auth.php';
