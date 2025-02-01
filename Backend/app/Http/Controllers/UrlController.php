c0f42e6dd2bee1d3f6fe655c7207ee7d63df6f7c
Błażej <wargun364@gmail.com>
2025-02-01 01:12:37 +0100

    url validation

--- Backend/app/Http/Controllers/UrlController.php
+++ Backend/app/Http/Controllers/UrlController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Url;
 use Illuminate\Http\Request;
 
 class UrlController extends Controller
 {
 
-    public function store(Request $request) {}
+    public function store(Request $request)
+    {
+        $request->validate([
+            'original_url' => 'required|url',
+        ]);
+
+        $shortenedUrl = 'shortened-url';
+
+
+
+        return response()->json(['shortener_url' => $shortenedUrl]);
+    }
 }
--- Frontend/src/pages/HomePage.vue
+++ Frontend/src/pages/HomePage.vue
@@ -5,13 +5,13 @@
           <div>
             <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
             <div class="mt-2">
-              <input v-model="data.title"  name="title" id="title" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+              <input v-model="data.title"  name="title" id="title" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
             </div>
           </div>
           <div>
             <label for="url" class="block text-sm/6 font-medium text-gray-900">Original URL</label>
             <div class="mt-2">
-              <input v-model="data.url"  name="url" id="url" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+              <input v-model="data.original_url" required  name="url" id="url" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
             </div>
           </div>
           <div>
